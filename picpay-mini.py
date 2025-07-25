fa4f46eee0a2cafcaca1d3cebea5c7e58196a7ca
oarthurcam <arthurcam2008@gmail.com>
2025-07-25 16:06:27 -0300

    Update picpay-mini.py
    apenas uns ajustes antes de terminar

--- picpay-mini.py
+++ picpay-mini.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-
+contador = 0
 caminho = os.path.join(os.getcwd(), "dados.json")
 
 sn = input('Voce tem uma conta? S/N: ').strip().lower()
@@ -22,12 +22,12 @@ def verificar(numero_frente, codigo):
             if dados[numero_frente]["codigo_seguranca"] == codigo:
                 print(f"Acesso autorizado para {dados[numero_frente]['nome_completo']}")
                 return True
-            else:
-                print("Código de segurança incorreto.")
         else:
-            print("Número do cartão não encontrado.")
+            print("Número do cartão ou código não encontrado.")
     
     return False
+
+
 
 # Sistema de entrar em conta
 while True:
@@ -35,19 +35,46 @@ while True:
         numero = input('Escreva o n° do cartão:\n ')
         cod_seg = input('Escreva o código de segurança:\n ')
         entrar = verificar(numero, cod_seg)
-
+        
+        # Usuário entro no sitema
         if entrar == True:
             print('Acesso permitido!')
-            break
+            
+            # Ação do usuário
+            while True:
+                escolha = int(input('(1) transferir\n(2) Investir\n(3) Sair \n Digite um numero para selecionar a opção: '))
+                
+                if escolha == 1:
+                    print()
+
+                elif escolha == 2:
+                    investir = input('Qual de nossas opções\n')
+
+                elif escolha == 3:
+                    break
+                else:
+                    print('Tente novamente...\n')
+                    contador += 1
+
+                    if contador > 2:
+                        break
+                
+        # Usuário errou os dados
         else:
             print('Acesso negado! \n')
-            print('Tente Novamente!\n')
+            print('Tente Novamente!')
+            contador += 1
+            
+            if contador > 2:
+                print('Houve um excesso de tentativas tente novamente mais tarde')
+                break
     
+    # Criar conta
     elif sn == 'n':
-        print('processo de criar conta') # inserir dados
-        break
+        print('processo de criar conta') 
+        
     
     else:
         print('Não entendi oque quis dizer')
         print('Tente novamente...')
-        
+
